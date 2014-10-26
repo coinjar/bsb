76e0f908fcddf23052f094679a5a7d54df85c70f
Ryan Zhou <git@zhoutong.com>
2014-10-26 22:33:26 +1100

    rename validator class to work with ActiveModel autoload

--- lib/bsb_number_validator.rb
+++ lib/bsb_number_validator.rb
@@ -1,7 +1,7 @@
 require 'bsb'
 require 'active_model'
 
-class BSBNumberValidator < ActiveModel::EachValidator
+class BsbNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless BSB.lookup(value)
       record.errors.add(attribute, options[:message] || :invalid)
