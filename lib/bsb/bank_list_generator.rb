ef40f8613094684369841680ff72989b74f05836
David Looi <david@coinjar.com>
2021-05-20 11:07:17 +1000

    replace restclient with net/http

--- lib/bsb/bank_list_generator.rb
+++ lib/bsb/bank_list_generator.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'rest-client'
 require 'csv'
 
 module BSB
@@ -14,8 +13,8 @@ module BSB
 
     def self.load_file(url)
       hash = {}
-      content = RestClient.get(url)
-      CSV.parse(content) do |row|
+      response = Net::HTTP.get(URI(url))
+      CSV.parse(response) do |row|
         row[2].split(", ").each do |prefix|
           prefix = prefix.chomp.rjust(2, "0")
           hash[prefix] = row[1]
