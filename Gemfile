227f91c6775d37367e66c2d947ddb315a8b802c5
gundamu-yunikon <94944771+gundamu-yunikon@users.noreply.github.com>
2024-05-02 15:58:25 +1000

    fix: discard date parsing, misc deps updates (#50)
    * misc dependency updates and version bumps
    
    * sort by modification time instead of filename stuff
    
    * rubocop fix
    
    * get rid of all the date code

--- Gemfile
+++ Gemfile
@@ -8,4 +8,4 @@ gemspec
 gem 'bundler', '~> 2.0'
 gem 'net-ftp', '~> 0.1.3'
 gem 'rake', '~> 13.0'
-gem 'rubocop', '~> 1.26'
+gem 'rubocop'
--- lib/auspaynet/client.rb
+++ lib/auspaynet/client.rb
@@ -4,8 +4,6 @@ require 'net/ftp'
 
 module Auspaynet
   class Client
-    MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec].freeze
-
     def initialize(host)
       @host = host
       @ftp = Net::FTP.new(@host)
@@ -23,35 +21,12 @@ module Auspaynet
     def list(dir:, matching_filename:, file_format: 'csv')
       @ftp.chdir(dir)
       files = @ftp.nlst.select do |f|
-        f.include?(matching_filename) &&
-          f.include?(file_format) &&
-          (f.include?("#{current_year}-") || f.include?("#{current_year})"))
+        f.include?(matching_filename) && f.end_with?(file_format)
       end
 
-      extract_latest_files(files: files, file_format: file_format)
+      files.sort_by { |fname| @ftp.mtime(fname) }
     ensure
       @ftp.chdir('/')
-    end
-
-    private
-
-    def current_year
-      Time.now.strftime('%y')
-    end
-
-    def extract_latest_files(files:, file_format:)
-      files.sort_by do |filename|
-        file_for_month(filename: filename, file_format: file_format)
-      end
-    end
-
-    def file_for_month(filename:, file_format:)
-      month_from_filename = filename.gsub(/(#{filename}|#{file_format}|\W|\d)/, '')
-      month_number(month_from_filename)
-    end
-
-    def month_number(month_from_filename)
-      MONTHS.find_index(month_from_filename)
     end
   end
 end
